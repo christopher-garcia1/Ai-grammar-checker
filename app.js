6a6e4bdffb7801231dd17c8844a61073ad286910
christopher garcia <71852007+christopher-garcia1@users.noreply.github.com>
2025-09-23 20:27:31 +0700

    house keeping settting up server and files

---
+++ app.js
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import express from 'express';
+import fetch from 'node-fetch';
+
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true })) 
