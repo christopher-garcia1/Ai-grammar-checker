6159ba4e87409e50883bcf1bc5a84f2984437f96
christopher garcia <71852007+christopher-garcia1@users.noreply.github.com>
2025-09-23 21:54:06 +0700

    used bootstrap and font awesome for front end

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ import fetch from 'node-fetch';
 
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
+ 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true })) 
+
+app.get('/', (req, res) => {
+    res.render('index')
+}) 
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
